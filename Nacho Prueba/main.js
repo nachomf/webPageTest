6dca0c7266933600b5e8241cba6fa892ad160c05
ignacio <nacho.emf@gmail.com>
2020-08-03 22:13:05 -0300

    fix error

--- Nacho Prueba/main.js
+++ Nacho Prueba/main.js
@@ -1,9 +1,22 @@
   document.getElementById('currentDate').innerHTML = new Date().toDateString();
 
 function turnTheLightOn() {
-  document.getElementById('myImage').innerHTML.src = "https://www.w3schools.com/js/pic_bulbon.gif";
+  document.getElementById('myImage').src = "https://www.w3schools.com/js/pic_bulbon.gif";
 };
 
 function turnTheLightOff() {
-  document.getElementById('myImage').innerHTML.src = "https://www.w3schools.com/js/pic_bulboff.gif";
+  document.getElementById('myImage').src = "https://www.w3schools.com/js/pic_bulboff.gif";
 };
+
+function displayAlert(msg){
+  window.alert(msg)
+};
+
+/*var person = {
+  firstName: "John",
+  lastName : "Doe",
+  id       : 5566,
+  fullName : function() {
+    return this.firstName + " " + this.lastName;
+  }
+};NO SE GUARDA LOCALMENTE*/

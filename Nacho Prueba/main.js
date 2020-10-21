a39db5e839f0e5987ca63386af8c4e119cf436e4
ignacio <nacho.emf@gmail.com>
2020-10-20 22:42:28 -0300

    continue javascript course

--- Nacho Prueba/main.js
+++ Nacho Prueba/main.js
@@ -1,4 +1,7 @@
+window.addEventListener("load", function(){
   document.getElementById('currentDate').innerHTML = new Date().toDateString();
+  showJsonNames ();
+});
 
 function turnTheLightOn() {
   document.getElementById('myImage').src = "https://www.w3schools.com/js/pic_bulbon.gif";
@@ -20,3 +23,20 @@ function displayAlert(msg){
     return this.firstName + " " + this.lastName;
   }
 };NO SE GUARDA LOCALMENTE*/
+
+function showJsonNames() {
+  var divJsonValues = document.getElementById("jsonValues");
+  var json = document.getElementById("json").innerText;
+  json = JSON.parse(json.substring(json.indexOf("{"),json.length-1).replaceAll("+","").replaceAll("'",""));
+  var ul = document.createElement("ul");
+  ul.className = "jsonValuesUl";
+  json.employees.forEach(employee => {
+    var li = document.createElement("li");
+    li.className = "jsonValuesLi";
+    li.innerText = employee.firstName + " " + employee.lastName;
+    
+    ul.appendChild(li);
+  });
+
+  divJsonValues.appendChild(ul);
+}
